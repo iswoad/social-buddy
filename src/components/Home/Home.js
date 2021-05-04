a87be252d7a0575dbe78f10991b4443b6ec8c72e
iswoad <iswoadansary@gmail.com>
2021-05-05 01:59:11 +0600

    react route social buddy initial commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import Post from '../Post/Post';
+
+const Home = () => {
+    const [posts, setPosts] = useState([]);
+    useEffect( () =>{
+        const url = `https://jsonplaceholder.typicode.com/posts`;
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setPosts(data))
+    }, [])
+    return (
+        <div>
+            <h1>This is the number of posts: {posts.length}</h1>
+            {
+                posts.map( post => <Post post={post}></Post>)
+            }
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/Post/Post.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useHistory } from 'react-router';
+
+const Post = (props) => {
+    const {id, title, body} = props.post;
+    const history = useHistory();
+    const showComments = id =>{
+        const url = `/post/${id}`
+        history.push(url);
+    }
+    
+    return (
+        <div>
+            <h4>Id: {id}</h4>
+            <h2>Title: {title}</h2>
+            <h3>{body}</h3>
+            <button onClick = {() => showComments(id)}>See Post</button>
+        </div>
+    );
+};
+
+export default Post;
